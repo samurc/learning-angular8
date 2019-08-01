33d6d95948ea370cf7317fc862dd59cdc76ff8f8
Samuel Cusi <scusi@gruporpp.com.pe>
2019-08-01 16:17:37 -0500

    IMPLEMENTANDO UN GUARD

--- src/app/services/authentication.guard.ts
+++ src/app/services/authentication.guard.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authenticationService.getStatus().pipe(
+      map(status => {
+        if (status) {
+          return true;
+        } else {
+          this.router.navigate(['login']);
+          return false;
+        }
+      })
+    );
   }
 }
